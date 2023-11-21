462b2b88409aa111aac6271b46d044f4de299f91
Tendrynyavo <143934345+tendrynyavo@users.noreply.github.com>
2023-11-21 12:39:07 +0300

    tailwind

--- basket-gui/src/App.js
+++ basket-gui/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
