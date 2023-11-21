b7d9cabfcdb04f1b50d661c6decab2a364a32fc0
ManoarySarobidy <manoarysarobidy@gmail.com>
2023-11-21 15:15:24 +0300

    adding spring back-end

--- statistics/src/main/java/com/dreamteam/statistics/controller/player/PlayerController.java
+++ statistics/src/main/java/com/dreamteam/statistics/controller/player/PlayerController.java
@@ -3,18 +3,26 @@ package com.dreamteam.statistics.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.*;
 
+
 import com.dreamteam.statistics.model.player.Player;
 import com.dreamteam.statistics.response.Response;
+import com.dreamteam.statistics.repository.*;
 
 @RestController
 @RequestMapping("/api/player")
 public class PlayerController{
+
 
 	@GetMapping
 	public Response getPlayers() throws Exception{
 		Player[] player = Player.getPlayersTable();
 		System.out.println("taille == " + player.length);
 		return new Response().addData("players" , player);
+	}
+
+	@PostMapping
+	public Response addPlayer( @RequestBody Player player ) throws Exception{
+			
 	}
 
 }
